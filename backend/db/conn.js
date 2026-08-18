4c0436f23677c89bbef065cf41a306670e5fb808
Marcos Davi <adamsdavi77@gmail.com>
2026-08-18 16:20:28 -0300

    Starts

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://adamsdavi77_db_user:EHuMHJLmNYL0ldBp@cluster0.pikydsj.mongodb.net/?appName=Cluster0",
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log("Conectado ao banco!");
   } catch (error) {
